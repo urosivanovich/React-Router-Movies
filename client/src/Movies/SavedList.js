28f8429eb0fd75f5c0aaab3c145551202ac17f4a
Uros Ivanovic <urosivanovic@Uross-MBP.lan>
2021-12-13 22:44:45 -0500

    useHistory hook used

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 export default function SavedList(props) {
+
+const  history = useHistory();
+
   return (
     <div className="saved-list">
       <h3>Saved Movies:</h3>
       {props.list.map(movie => (
         <span className="saved-movie">{movie.title}</span>
       ))}
-      <div className="home-button">
-        <Link to='/'>Home</Link>
+      <div className="home-button" onClick={() => history.push('/')}>
+        Home
         </div>
     </div>
   );
